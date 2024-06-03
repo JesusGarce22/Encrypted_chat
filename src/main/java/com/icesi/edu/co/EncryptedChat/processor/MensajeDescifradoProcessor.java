de877a409c18787b96f0ba40f08d3d9c38df6957
JesusGarce22 <jesus02garces@gmail.com>
2024-06-03 11:32:55 -0500

    Delete gradle project

--- src/main/java/com/icesi/edu/co/EncryptedChat/processor/MensajeDescifradoProcessor.java
+++
@@ -1,6 +0,0 @@
-package com.icesi.edu.co.EncryptedChat.processor;
-
-// Definition of the interface for the decrypted message processor
-public interface MensajeDescifradoProcessor {
-    void procesarMensaje(String mensajeDescifrado);
-}
